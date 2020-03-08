fae2a0fdb232d62e3f30b812eb6b979afed04762
weichichou <weichi.cc@gmail.com>
2020-03-08 15:57:08 +0100

    Set interval and change gif based on the state

--- main.js
+++ main.js
@@ -1,29 +1,52 @@
-var fsm = new StateMachine({
+var hungryFsm = new StateMachine({
   init: "normal",
   transitions: [
     { name: "feed", from: "hungry", to: "normal" },
     { name: "time", from: "normal", to: "hungry" },
-    { name: "longtime", from: "hungry", to: "dead" }
+    { name: "time", from: "hungry", to: "dead" },
+    { name: "time", from: "dead", to: "dead" }
   ],
   methods: {
     onFeed: function() {
       console.log("Eating");
+      document.getElementById("gif").src = "images/eating.png";
     }
-    /* onFreeze: function() {
-      console.log("I froze");
-    },
-    onVaporize: function() {
-      console.log("I vaporized");
-    },
-    onCondense: function() {
-      console.log("I condensed");
+    /*  onEnterHungry: function() {
+      document.getElementById("gif").src = "images/hungry.gif";
+    } */
+  }
+});
+
+var cleanFsm = new StateMachine({
+  init: "normal",
+  transitions: [
+    { name: "bath", from: "dirty", to: "normal" },
+    { name: "time", from: "normal", to: "dirty" },
+    { name: "time", from: "dirty", to: "dead" },
+    { name: "time", from: "dead", to: "dead" }
+  ],
+  methods: {
+    /* onFeed: function() {
+      console.log("Eating");
+      document.getElementById("gif").src = "images/eating.png";
     } */
   }
 });
 
 setInterval(() => {
-  console.log(fsm.state);
-  fsm.time();
-  console.log(fsm.state);
-}, 10000);
+  console.log(hungryFsm.state);
+  hungryFsm.time();
+  console.log(hungryFsm.state);
+}, 3000);
 /* Set interval as 10 seconds */
+
+setInterval(() => {
+  console.log(cleanFsm.state);
+  cleanFsm.time();
+  console.log(cleanFsm.state);
+}, 10000);
+
+setInterval(() => {
+  const filename = `${hungryFsm.state}-${cleanFsm.state}.gif`;
+  document.getElementById("gif").src = `images/${filename}`;
+}, 5000);
